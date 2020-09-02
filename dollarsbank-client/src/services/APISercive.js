cfccdf8f0de8ca61316c02a222f03e16ec697602
Keith <visually.basic.keith@gmail.com>
2020-09-02 10:31:52 -0400

    building api

--- dollarsbank-client/src/services/APISercive.js
+++ dollarsbank-client/src/services/APISercive.js
@@ -1,7 +1,8 @@
+import TokenService from './TokenService';
 
 const APIService = {
 
-    sendLogin(username, password){
+    postLogin(username, password){
         const postOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application.json'},
@@ -14,6 +15,76 @@ const APIService = {
         .then ( res => {
             //do something here
             //set user ID into token service
+        });
+    },
+    postDeposit(amount){
+        const postOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application.json'},
+            body: {
+                'amount': amount,
+                'id': TokenService.getUserID()
+            }
+        };
+        return fetch ('URL HERE', postOptions)
+        .then ( res => {
+            //display error or success
+        });
+    },
+    postWithdraw(amount){
+        const postOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application.json'},
+            body: {
+                'amount': amount,
+                'id': TokenService.getUserID()
+            }
+        };
+        return fetch ('URL HERE', postOptions)
+        .then ( res => {
+            //display error or success
+        });
+    },
+    postTransfer(amount, targetID){
+        const postOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application.json'},
+            body: {
+                'amount': amount,
+                'id': TokenService.getUserID(),
+                'targetID': targetID
+            }
+        };
+        return fetch ('URL HERE', postOptions)
+        .then ( res => {
+            //display error or success
+        });
+    },
+    postNewUser(username, password, amount){
+        const postOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application.json'},
+            body: {
+                'username': username,
+                'password': password,
+                'amount': amount
+            }
+        };
+        return fetch ('URL HERE', postOptions)
+        .then ( res => {
+            //display error or success
+        });
+    },
+    getRecent(id){
+        //this one im less certain on that the post ones
+        const getOptions = {
+            method: 'GET',
+            headers: {'Content-Type': 'application.json'},
+            body: {}
+        };
+        return fetch ('URL HERE', getOptions)
+        .then ( res => {
+            //extract data for rendering
         });
     }
 }
